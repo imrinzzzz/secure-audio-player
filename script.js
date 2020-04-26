daa926ec763cff80966dd61c8c560c426c61ec48
thanirin <thanirin.tri@student.mahidol.ac.th>
2020-04-26 12:09:38 +0700

    finally the JS works

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(function () {
     back = $('a.start');
 
     // page 1
-    $('#page1 .encrypt').click(function() {
+    $('#page1 .encrypt').on("click", function() {
         body.attr('class', 'encrypt');
         page(2);
     });
@@ -15,6 +15,33 @@ $(function () {
 
     // page 2
     $('#page2 .button').click(function() {
-        
-    })
-})
+        $(this).parent().find('input').click()
+    });
+
+    var file = null;
+
+    $('#page2').on('change', '#encrypt-input', function(f) {
+
+        file = f.target.files;
+
+        if(file.length!=1) {
+            alert('Please select a file to encrypt!!');
+            return false;
+        } else {
+            page(3);
+        }
+    });
+
+
+    // HELPER FUNCTION
+    function page(i) {
+        if(i == 1) {
+            back.fadeOut();
+        } else {
+            back.fadeIn();
+        }
+        // move #stage (chaning the top property will trigger a css transition on element)
+        // i-1 so the page will start from 1:
+        stage.css('top', (-(i-1)*100)+'%');
+    }
+});
