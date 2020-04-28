3be635c8f47d1d05b76342611a6b18f8d72bf2d0
thanirin <thanirin.tri@student.mahidol.ac.th>
2020-04-28 10:45:21 +0700

    encrypt decrypt done (no file created yet)

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ $(function () {
             alert('Please select a file to encrypt!!');
             return false;
         } else {
-            alert('The file "' + file.name + '" has been selected.');
+            alert('The file "' + file[0].name + '" has been selected.');
             page(3);
         }
     });
@@ -54,54 +54,39 @@ $(function () {
     $('a.button.process').click(function () {
 
         let fr = new FileReader();
+
         if (body.hasClass('encrypt')) {
-            fr.onload = function () {
+            fr.onload = async function () {
                 var arrayBuffer = fr.result;
-                console.log("arrayBuffer: ");
-                console.log(arrayBuffer);
-                audioContext.decodeAudioData(arrayBuffer, decodedDone);
+                var wordArray = CryptoJS.lib.WordArray.create(arrayBuffer);
+                var encr = CryptoJS.AES.encrypt(wordArray, password)
+                // console.log(encr)
+                // audioContext.decodeAudioData(arrayBuffer, decodedDone);
             };
             fr.readAsArrayBuffer(file[0]);
 
-            //     fr.onload = function (e) {
-            //         // textread = e.target.result;
-            //         // enctext = UU.callEncrypt(textread);
-            //         var encrypted = CryptoJs.AES.encrypt(e.target.result, password);
-            //         a.attr('href', 'data:application/octet-stream,' + encrypted);
-            //         a.attr('download', file.name + '.encrypted');
-            //     }
-            //     fr.readAsDataURL(file);
-            // } else if(body.hasClass('decrypt')) {
-            //     fr.onload = function(e) {
-            //         var decrypted = CryptoJS.AES.decrypt(e.target.result, password).toString(CryptoJS.enc.Latin1);
-            //         a.attr('href', decrypted);
-            //         a.attr('download', file.name.replace('.encrypted', ''));
-            //     }
-            //     fr.readAsText(file);
         } else if (body.hasClass('decrypt')) {
             fr.onload = function () {
                 var arrayBuffer = fr.result;
-                console.log("arrayBuffer: ");
-                console.log(arrayBuffer);
-                audioContext.decodeAudioData(arrayBuffer, play);
+                var decr = CryptoJS.AES.decrypt(arrayBuffer, password)
+                // console.log(arrayBuffer);
+                var arr = base64DecToArr(decr.toString(CryptoJS.enc.Base64));
+                audioContext.decodeAudioData(arr.buffer, (buffer) => {
+                    alert('success decoding buffer');
+                    // play(buffer);
+                }, (err) => { alert('couldn\'t decode buffer');});
             };
             fr.readAsArrayBuffer(file[0]);
         }
 
     });
 
-    function play(decoded) {
-        const source = audioContext.createBufferSource();
-        source.buffer = decoded;
-        source.connect(audioContext.destination);
-        source.start();
-    }
-
     function decodedDone(decoded) {
-        var typedArray = new Float32Array(decoded.length);
-        typedArray = decoded.getChannelData(0);
-        console.log("typedArray: ");
-        console.log(typedArray);
+        // var typedArray = new Float32Array(decoded.length);
+        // typedArray = decoded.getChannelData(0);
+        // console.log("typedArray: ");
+        // console.log(typedArray);
+        // console.log(decoded)
     }
 
     // ================================== OTHER ================================== //
@@ -114,5 +99,48 @@ $(function () {
         // move #stage (chaning the top property will trigger a css transition on element)
         // i-1 so the page will start from 1:
         stage.css('top', (-(i - 1) * 100) + '%');
+    }
+
+    function play(decoded) {
+        const source = audioContext.createBufferSource();
+        source.buffer = decoded;
+        source.connect(audioContext.destination);
+        source.start();
+    }
+
+    function b64ToUint6(nChr) {
+        return nChr > 64 && nChr < 91 ?
+            nChr - 65 :
+            nChr > 96 && nChr < 123 ?
+            nChr - 71 :
+            nChr > 47 && nChr < 58 ?
+            nChr + 4 :
+            nChr === 43 ?
+            62 :
+            nChr === 47 ?
+            63 :
+            0;
+    }
+
+    function base64DecToArr(sBase64, nBlocksSize) {
+        var
+            sB64Enc = sBase64.replace(/[^A-Za-z0-9\+\/]/g, ""),
+            nInLen = sB64Enc.length,
+            nOutLen = nBlocksSize ? Math.ceil((nInLen * 3 + 1 >> 2) / nBlocksSize) * nBlocksSize : nInLen * 3 + 1 >> 2,
+            taBytes = new Uint8Array(nOutLen);
+
+        for (var nMod3, nMod4, nUint24 = 0, nOutIdx = 0, nInIdx = 0; nInIdx < nInLen; nInIdx++) {
+            nMod4 = nInIdx & 3;
+            nUint24 |= b64ToUint6(sB64Enc.charCodeAt(nInIdx)) << 18 - 6 * nMod4;
+            if (nMod4 === 3 || nInLen - nInIdx === 1) {
+                for (nMod3 = 0; nMod3 < 3 && nOutIdx < nOutLen; nMod3++, nOutIdx++) {
+                    taBytes[nOutIdx] = nUint24 >>> (16 >>> nMod3 & 24) & 255;
+                }
+                nUint24 = 0;
+
+            }
+        }
+
+        return taBytes;
     }
 });
